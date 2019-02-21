babdb9d13bcd5295baf63d1bdfad027754b64818
Craig Ramsay <craig.ramsay.100@strath.ac.uk>
2019-02-21 14:18:29 -0700

    Update overlay for 2018.3 drivers

--- PynqQPSK/QpskOverlay.py
+++ PynqQPSK/QpskOverlay.py
@@ -12,7 +12,7 @@ class QpskOverlay(Overlay):
         # Generate default bitfile name
         if bitfile_name is None:
             this_dir = os.path.dirname(__file__)
-            bitfile_name = os.path.join(this_dir, 'base.bit')
+            bitfile_name = os.path.join(this_dir, 'bitstream', 'base.bit')
 
         # Set FPD and LPD interface widths
         from pynq import MMIO
@@ -33,28 +33,35 @@ class QpskOverlay(Overlay):
         self.adc_block = self.adc_tile.blocks[0]
 
         # Start up LMX clock
-        xrfclk_set_freq(409.6)
+        xrfclk.set_all_ref_clks(409.6)
 
         # Set sane DAC defaults
         self.dac_tile.ShutDown()
-        self.dac_tile.DynamicPLLConfig(xrfdc.XRF_CLK_SRC_PLL,409.6,1024)
+        self.dac_tile.DynamicPLLConfig(1,409.6,1024)
         self.dac_block.MixerSettings = {
-           "Freq": 84,
-           "CoarseMixFreq": xrfdc.XRF_DC_COARSE_MIX_SAMPLE_FREQ_BY_TWO,
-           "CoarseMixMode": xrfdc.XRF_DC_COARSE_MIX_MODE_C2C_C2R,
-           "FineMixerMode": xrfdc.XRF_DC_FINE_MIXER_MOD_COMPLX_TO_REAL}
+                'CoarseMixFreq':  0x10, #COARSE_MIX_BYPASS
+                'EventSource':    0x00, #IMMEDIATE
+                'FineMixerScale': 0x01, #SCALE_1P0
+                'Freq':           84.0,
+                'MixerMode':      0x02, #MODE_C2R
+                'MixerType':      0x02, #TYPE_FINE         
+                'PhaseOffset':     0.0
+        }
         self.dac_tile.SetupFIFO(True)
         self.dac_tile.StartUp()
 
         # Set sane ADC defaults
         self.adc_tile.ShutDown()
-        self.adc_tile.DynamicPLLConfig(xrfdc.XRF_CLK_SRC_PLL,409.6,1024)
+        self.adc_tile.DynamicPLLConfig(1,409.6,1024)
         self.adc_block.MixerSettings = {
-           "Freq": 84.000,
-           "CoarseMixFreq": xrfdc.XRF_DC_COARSE_MIX_SAMPLE_FREQ_BY_TWO,
-           "CoarseMixMode": 2,
-           "FineMixerMode": 3,
-           "EventSource": 2}
+                'CoarseMixFreq':  0x10, #COARSE_MIX_BYPASS
+                'EventSource':    0x02, #TILE
+                'FineMixerScale': 0x01, #SCALE_1P0
+                'Freq': 84.0,
+                'MixerMode':      0x03, #MODE_R2C
+                'MixerType':      0x02, #TYPE_FINE
+                'PhaseOffset': 0.0
+        }
         self.adc_tile.SetupFIFO(True)
         self.adc_tile.StartUp()
 
