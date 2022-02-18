75e00b42e5c5323dd7902ba16057cf1fa20c24f4
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2022-02-18 11:15:07 +0000

    Add new bitstream, hwh, and block design for 2020.2

--- boards/RFSoC2x2/rfsoc_qpsk/make_bitstream.tcl
+++ boards/RFSoC2x2/rfsoc_qpsk/make_bitstream.tcl
@@ -7,7 +7,7 @@ open_bd_design ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name
 
 # Add top wrapper and xdc files
 make_wrapper -files [get_files ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/${design_name}.bd] -top
-add_files -norecurse ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/hdl/${design_name}_wrapper.vhd
+add_files -norecurse ./${overlay_name}/${overlay_name}.gen/sources_1/bd/${design_name}/hdl/${design_name}_wrapper.vhd
 set_property top ${design_name}_wrapper [current_fileset]
 update_compile_order -fileset sources_1
 
@@ -16,7 +16,7 @@ launch_runs impl_1 -to_step write_bitstream -jobs 4
 wait_on_run impl_1
 
 # Move and rename bitstream to final location
-file copy -force ./${overlay_name}/${overlay_name}.runs/impl_1/${design_name}_wrapper.bit ./overlay/${overlay_name}.bit
+file copy -force ./${overlay_name}/${overlay_name}.runs/impl_1/${design_name}_wrapper.bit ./bitstream/${overlay_name}.bit
 
 # copy hwh files
-file copy -force ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/hw_handoff/${design_name}.hwh ./overlay/${overlay_name}.hwh
+file copy -force ./${overlay_name}/${overlay_name}.gen/sources_1/bd/${design_name}/hw_handoff/${design_name}.hwh ./bitstream/${overlay_name}.hwh
