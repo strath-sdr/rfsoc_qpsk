8a499e3c6d40b9e452cd42e48b50440419691a3f
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2022-06-28 10:57:01 +0100

    Add RFSoC4x2 support

---
+++ boards/RFSoC4x2/rfsoc_qpsk/make_block_design.tcl
@@ -0,0 +1,17 @@
+set overlay_name "block_design"
+set design_name "rfsoc_qpsk"
+set iprepo_dir ./../../ip/iprepo
+
+# Create project
+create_project ${overlay_name} ./${overlay_name} -part xczu48dr-ffvg1517-2-e
+set_property target_language VHDL [current_project]
+
+# Set IP repository paths
+set_property ip_repo_paths $iprepo_dir [current_project]
+update_ip_catalog
+
+# Add constraints
+add_files -fileset constrs_1 -norecurse ./constraints.xdc
+
+# Make block design
+source ./${design_name}.tcl
